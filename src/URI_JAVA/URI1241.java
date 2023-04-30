e96a00a3a9b6a82c7abe7f4894795801f66986fd
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-04-30 16:35:04 -0300

    Create URI1241.java

---
+++ src/URI_JAVA/URI1241.java
@@ -0,0 +1,20 @@
+package URI_JAVA;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class URI1241 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        for (int i = 0; i < N; i++) {
+            String A = leitor.next();
+            String B = leitor.next();
+            if (A.endsWith(B))
+                System.out.println("encaixa");
+            else
+                System.out.println("nao encaixa");
+        }
+    }
+}
