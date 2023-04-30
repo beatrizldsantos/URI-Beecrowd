50d6efd023814faf1d927b809b7b6d88e2f9ea65
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-04-30 19:02:38 -0300

    30/04/2023

---
+++ src/URI_JAVA/URI1011.java
@@ -0,0 +1,32 @@
+package URI_JAVA;
+
+import java.util.Scanner;
+
+public class URI1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+
+            String board = sc.next();
+
+            // Verifica se Maria pode sempre ganhar
+            boolean mariaWins = false;
+            for (int i = 0; i <= n - 3; i++) {
+                if (board.charAt(i) == '.' && board.charAt(i+1) == '.' && board.charAt(i+2) == '.') {
+                    mariaWins = true;
+                    break;
+                }
+            }
+
+            // Imprime resultado
+            System.out.println(mariaWins ? "S" : "N");
+        }
+
+        sc.close();
+    }
+}
