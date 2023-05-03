2b58df26b4258b0789e66cfa5997ee2ae703ced0
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-05-03 20:54:13 -0300

    03/05/2023

---
+++ src/URI_JAVA/URI_2839.java
@@ -0,0 +1,10 @@
+package URI_JAVA;
+import java.io.IOException;
+import java.util.Scanner;
+public class URI_2839 {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int meias = leitor.nextInt();
+        System.out.println(meias + 1);
+    }
+}
