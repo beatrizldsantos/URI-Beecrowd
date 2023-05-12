b3ca53cbf16b258bba6bc515fdceffb9f833e5e2
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-05-12 20:48:45 -0300

    Atualizar o URI1005.java

--- src/URI_JAVA/URI1005.java
+++ src/URI_JAVA/URI1005.java
@@ -2,6 +2,7 @@ package URI_JAVA;
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class URI1005 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
