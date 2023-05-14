81305228c528c0a83182f15acbf249c8c8204ed5
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-05-13 22:43:08 -0300

    13/05/2023

---
+++ src/URI_JAVA/URI1060.java
@@ -0,0 +1,16 @@
+package URI;
+import java.util.Scanner;
+public class URI1060 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int c = 0;
+        for (int i = 0; i < 6; i++) {
+            double n = ler.nextDouble();
+            if (n > 0) {
+                c++;
+            }
+        }
+
+        System.out.printf("%d valores positivos\n", c);
+    }
+}
---
+++ src/URI_JAVA/URI1064.java
@@ -0,0 +1,25 @@
+package URI;
+import java.util.Scanner;
+public class URI1064 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] nums = new double[6];
+        int numPositives = 0;
+        double sumPositive = 0.0;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextDouble();
+            if (nums[i] > 0) {
+                numPositives++;
+                sumPositive += nums[i];
+            }
+        }
+
+        double avgPositive = sumPositive / numPositives;
+
+        System.out.println(numPositives + " valores positivos");
+        System.out.printf("%.1f%n", avgPositive);
+
+        sc.close();
+    }
+}
