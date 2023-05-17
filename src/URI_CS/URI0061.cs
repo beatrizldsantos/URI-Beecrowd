a79a4e66bd646926a5d2ec6e82d63294bca54140
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-05-16 22:11:25 -0300

    Update URI0061.cs

--- src/URI_CS/URI0061.cs
+++ src/URI_CS/URI0061.cs
@@ -17,7 +17,6 @@ class URI {
             Console.WriteLine(sum);
         }
     }
-
     static BigInteger Factorial(int n) {
         BigInteger result = 1;
         for (int i = 2; i <= n; i++) {
