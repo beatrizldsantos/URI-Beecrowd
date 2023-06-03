3eb63ede37a7f57c6c0a0540717ed46790e4eff1
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-06-03 17:32:27 -0300

    Resoluções

---
+++ src/URI_JAVA/URI1070.java
@@ -0,0 +1,23 @@
+package URI_JAVA;
+import java.util.Scanner;
+public class URI1070 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um valor inteiro: ");
+        int x = scanner.nextInt();
+
+        int contador = 0;
+        int numeroImpar = x;
+
+        while (contador < 6) {
+            if (numeroImpar % 2 != 0) {
+                System.out.println(numeroImpar);
+                contador++;
+            }
+            numeroImpar++;
+        }
+
+        scanner.close();
+    }
+}
