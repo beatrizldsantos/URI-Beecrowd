fa08d27ec134ee8524591ff123cb5e445e9b3693
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-06-08 13:37:53 -0300

    Update 2605.sql

--- src/URI_SQL/2605.sql
+++ src/URI_SQL/2605.sql
@@ -1,4 +1,4 @@
-SELECT products.name, providers.name 
+SELECT products.name,providers.name 
 FROM products 
 INNER JOIN providers ON products.id_providers = providers.id 
 WHERE products.id_categories = 6;
