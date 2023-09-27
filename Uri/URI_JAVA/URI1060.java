fdb05fdc83e1f9ab7c83e48214a1d2f3d0f4ad7e
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-09-26 22:40:03 -0300

    Atualizar o URI1060.java

--- Uri/URI_JAVA/URI1060.java
+++ Uri/URI_JAVA/URI1060.java
@@ -4,6 +4,7 @@ public class URI1060 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         int c = 0;
+
         for (int i = 0; i < 6; i++) {
             double n = ler.nextDouble();
             if (n > 0) {
