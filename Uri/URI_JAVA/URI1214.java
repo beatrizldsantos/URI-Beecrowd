726b170006a13c7e854e548870e1e1e59b507dcb
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-10-13 10:09:46 -0300

    Create URI1214.java

---
+++ Uri/URI_JAVA/URI1214.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class URI1214 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();
+
+        DecimalFormat df = new DecimalFormat("0.000");
+
+        for (int i = 0; i < C; i++) {
+            int N = sc.nextInt();
+            int[] notas = new int[N];
+            int soma = 0;
+
+            for (int j = 0; j < N; j++) {
+                notas[j] = sc.nextInt();
+                soma += notas[j];
+            }
+
+            int media = soma / N;
+            int acimaMedia = 0;
+
+            for (int j = 0; j < N; j++) {
+                if (notas[j] > media) {
+                    acimaMedia++;
+                }
+            }
+
+            double percentual = (acimaMedia * 100.0) / N;
+            System.out.println(df.format(percentual) + "%");
+        }
+
+        sc.close();
+    }
+}
