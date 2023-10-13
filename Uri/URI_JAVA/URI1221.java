fb4bd7cf11ecff9544e7e11b8d98447932fdb89a
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-10-13 10:05:20 -0300

    Create URI1221.java

---
+++ Uri/URI_JAVA/URI1221.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class URI1221 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int X = scanner.nextInt();
+            if (isPrime(X)) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
+            }
+        }
+    }
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
