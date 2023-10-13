2a818fa8e8c9ce2f048d2454a10c73473ca61df8
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-10-13 10:11:03 -0300

    Create URI1219.java

---
+++ Uri/URI_JAVA/URI1219.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class URI1219 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+
+            double[] areas = calcAreas(a, b, c);
+            System.out.printf("%.4f %.4f %.4f%n", areas[0], areas[1], areas[2]);
+        }
+    }
+
+    public static double[] calcAreas(double a, double b, double c) {
+        double[] result = new double[3];
+        double raioCirculoInterno;
+        double raioCirculoExterno, semiPerimetro;
+
+        raioCirculoExterno = (a * b * c) / Math.sqrt((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c));
+        semiPerimetro = (a + b + c) / 2;
+
+        result[1] = Math.sqrt(semiPerimetro * ((semiPerimetro - a) * (semiPerimetro - b) * (semiPerimetro - c)));
+        raioCirculoInterno = result[1] / semiPerimetro;
+        result[0] = (raioCirculoExterno * raioCirculoExterno * Math.PI) - result[1];
+        result[2] = (raioCirculoInterno * raioCirculoInterno) * Math.PI;
+        result[1] = result[1] - result[2];
+
+        return result;
+    }
+}
