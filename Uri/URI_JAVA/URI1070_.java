5100d82bf91d75ddff382f0b199fd95538d45c8b
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-10-26 22:07:38 -0300

    Create URI1070_.java

---
+++ Uri/URI_JAVA/URI1070_.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class URI1070_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = x; i < x + 12; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
