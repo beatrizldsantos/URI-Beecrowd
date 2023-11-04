88715f03a90202fb69d8f824d7cf4bc155a50682
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-11-03 21:23:47 -0300

    Create URI1845.c

---
+++ Uri/URI1845.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+ 
+int main()
+{
+    char aux , i , f[8] ={'f' ,'s', 'j', 'b', 'z', 'p', 'v', 'x'} , F[8] = {'F' , 'S' , 'J' , 'B' , 'Z' , 'P' , 'V' , 'X'} ;
+	char letra;
+	int ant_f = 0;
+	int eh_f = 0; 
+	while(scanf("%c" , &letra) != EOF)
+	{
+		eh_f = 0;
+		for(i = 0; i < 8 ; i++)
+		{
+			if(letra == f[i]) {
+				letra = 'f';
+				eh_f = 1;
+			};
+			if(letra == F[i]) {
+				letra = 'F';
+				eh_f = 1;
+			};
+		}
+		if (eh_f == 0)  
+			printf("%c" , letra);
+		else 
+		{
+			if (ant_f == 0) 
+				printf("%c" , letra);	
+			
+		}
+		if (eh_f == 1) 
+			ant_f = 1;
+	    else 
+			ant_f = 0;
+		
+	}
+	
+    return 0;
+}
