327b388a54277a24f7c05e006c01d971637f851f
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-11-16 15:24:18 -0300

    Create URI1071.java

---
+++ Uri/URI_JAVA/URI1071.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class URI1071 {
+	
+    public static void main(String[] args) throws IOException {
+    	Scanner leitor = new Scanner(System.in);
+    	int soma = 0;
+		int X = leitor.nextInt();
+		int Y = leitor.nextInt();
+
+		if (X < Y) {
+			for (int i = X + 1; i < Y; i++) {
+				if (i % 2 != 0) {
+					soma += i;
+				}
+			}
+		} else {
+			for (int i= Y + 1; i < X; i++) {
+				if (i % 2 != 0) {
+					soma += i;
+				}
+			}
+		}
+		System.out.println(soma);
+    }
+	
+}
