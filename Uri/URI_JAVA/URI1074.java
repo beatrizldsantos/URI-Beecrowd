9af5c5047e9b9ae77814308d846bc6fd53e93a36
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-11-16 15:25:14 -0300

    Create URI1074.java

---
+++ Uri/URI_JAVA/URI1074.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int X = scanner.nextInt();
+            classifyNumber(X);
+        }
+
+        scanner.close();
+    }
+
+    public static void classifyNumber(int number) {
+        if (number == 0) {
+            System.out.println("NULL");
+        } else {
+            String parity = (number % 2 == 0) ? "EVEN" : "ODD";
+            String sign = (number > 0) ? "POSITIVE" : "NEGATIVE";
+
+            System.out.println(parity + " " + sign);
+        }
+    }
+}
