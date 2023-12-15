c87dcb019ef811a6a85833b88366f440900013b2
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-12-15 20:23:14 -0300

    Create URI1262.java

---
+++ Uri/URI_JAVA/URI1262.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k;
+        String string;
+
+        while (scanner.hasNext()) {
+            string = scanner.next();
+            k = scanner.nextInt();
+            process(string, k);
+        }
+    }
+
+    public static void process(String str, int n) {
+        int i = 0, ans = 0, cnt = 0;
+
+        while (i < str.length()) {
+            if (str.charAt(i) == 'W') {
+                ans++;
+                i++;
+            } else if (str.charAt(i) == 'R' && i + 1 < str.length() && str.charAt(i + 1) == 'W') {
+                ans++;
+                i++;
+            } else if (str.charAt(i) == 'R' && i + 1 < str.length() && str.charAt(i + 1) == 'R') {
+                while (i < str.length() && str.charAt(i) == 'R' && cnt < n) {
+                    cnt++;
+                    i++;
+                }
+                ans++;
+                cnt = 0;
+            } else {
+                ans++;
+                i++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
