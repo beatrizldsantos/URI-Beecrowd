f29772949c088c43ac6e3e0edb4b92a674655f86
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-12-17 12:56:08 -0300

    Create URI3040.cs

---
+++ Uri/URI_CS/URI3040.cs
@@ -0,0 +1,44 @@
+using System;
+
+{
+    class URI3040
+    {
+        static void Main(string[] args)
+        {
+
+            int ts = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ts; i++)
+            {
+                string[] v = Console.ReadLine().Split();
+
+                int altura = int.Parse(v[0]);
+                int diametro = int.Parse(v[1]);
+                int galhos = int.Parse(v[2]);
+
+                if(altura >= 200 && altura <= 300)
+                {
+                    if (diametro >= 50)
+                    {
+                        if(galhos > 150)
+                        {
+                            Console.WriteLine("Sim");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nao");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nao");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nao");
+                }
+            }
+        }
+    }
+}
