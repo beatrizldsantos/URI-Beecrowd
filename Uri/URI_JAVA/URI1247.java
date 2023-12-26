95e2ad2292c1f4fa456b9d97dbec52b086d7b917
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2023-12-26 20:28:41 -0300

    1247 - Guarda Costeira

---
+++ Uri/URI_JAVA/URI1247.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class URI1247 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int D = scanner.nextInt();
+            int VF = scanner.nextInt();
+            int VG = scanner.nextInt();
+
+            if (podeCapturarLadrao(D, VF, VG)) {
+                System.out.println("S");
+            } else {
+                System.out.println("N");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static boolean podeCapturarLadrao(int D, int VF, int VG) {
+        double distanciaGuardaCosteira = Math.sqrt(D * D + 144);
+        double tempoFugitivo = 12.0 / VF;
+        double tempoGuardaCosteira = distanciaGuardaCosteira / VG;
+
+        return tempoGuardaCosteira <= tempoFugitivo;
+    }
+}
