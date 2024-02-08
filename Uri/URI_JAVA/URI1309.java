edb823de29c84054c020be83a51a52d6228e39b1
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-08 18:26:58 -0300

    3299 - Números Má Sorte Pequenos

---
+++ Uri/URI_JAVA/URI1309.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class URI1309 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int dolares = scanner.nextInt();
+            int centavos = scanner.nextInt();
+            System.out.println(formatarMoeda(dolares, centavos));
+        }
+    }
+
+    public static String formatarMoeda(int dolares, int centavos) {
+        DecimalFormat df = new DecimalFormat("#,##0.00", new DecimalFormatSymbols(Locale.US));
+        String valorFormatado = df.format(dolares + centavos / 100.0);
+        return "$" + valorFormatado;
+    }
+}
