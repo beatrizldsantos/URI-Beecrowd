ff888cf620524112e7f65ba3c5a81da535fc8a1b
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-19 19:37:58 -0300

    1145 - Sequência Lógica 2

---
+++ Uri/URI_JAVA/URI1145.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class URI1145 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        
+        for (int i = 1; i <= Y; i++) {
+            System.out.print(i);
+            if (i % X == 0) {
+                System.out.println();
+            } else {
+                System.out.print(" ");
+            }
+        }
+        
+        sc.close();
+    }
+}
