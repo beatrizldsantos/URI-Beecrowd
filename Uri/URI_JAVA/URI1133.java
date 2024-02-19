3eca208c26cdf933b3ecc2802dec3ff0de8775dd
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-19 19:21:36 -0300

    1133 - Resto da Divisão

---
+++ Uri/URI_JAVA/URI1133.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class URI1133 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        
+        if (X > Y) {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+        
+        for (int i = X + 1; i < Y; i++) {
+            if (i % 5 == 2 || i % 5 == 3) {
+                System.out.println(i);
+            }
+        }
+        
+        sc.close();
+    }
+}
