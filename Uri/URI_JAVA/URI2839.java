135ed890b1cc9e5a4796c41b06225f497b338576
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-20 14:06:50 -0300

    2839 - As Meias de Rangel

--- Uri/URI_JAVA/URI2839.java
+++ Uri/URI_JAVA/URI2839.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class URI2839 {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
+
         int meias = leitor.nextInt();
         System.out.println(meias + 1);
     }
