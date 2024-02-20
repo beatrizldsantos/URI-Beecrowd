44d05abcf1abfecd25e06700eb19c5b6be1076cd
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-20 13:34:08 -0300

    1958 - Notação Científica

--- Uri/URI_JAVA/URI1958.java
+++ Uri/URI_JAVA/URI1958.java
@@ -1,10 +1,13 @@
+package URI_JAVA;
+
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class URI1958 {
     public static void main(String[] args) throws IOException {
-		Scanner leitor = new Scanner(System.in);
-		double X = leitor.nextDouble();
+		Scanner sc = new Scanner(System.in);
+		double X = sc.nextDouble();
 		System.out.println(String.format((String.valueOf(X).startsWith("-") ? "" : "+") + "%.4E", X));
     }
+}
 	
