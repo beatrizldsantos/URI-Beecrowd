c9d4ab7122c02639fd9729f04f5a16c09f332e0c
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-20 12:59:48 -0300

    1047 - Tempo de Jogo com Minutos

--- Uri/URI_JAVA/URI1047.java
+++ Uri/URI_JAVA/URI1047.java
@@ -1,5 +1,6 @@
+package URI_JAVA;
 import java.util.Scanner;
-public class Main {
+public class URI1047 {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
         int h1 = entrada.nextInt();
