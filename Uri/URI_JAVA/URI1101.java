f4f0762857d8236d71c9f22291bb1679cfba76f0
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-02-20 13:06:10 -0300

    1101 - Sequência de Números e Soma

--- Uri/URI_JAVA/URI1101.java
+++ Uri/URI_JAVA/URI1101.java
@@ -1,19 +1,19 @@
 package URI_JAVA;
 import java.util.Scanner;
 public class URI1101{
-    public static void main(String[] args) {
+    public static void main(String[] args, int i) {
         int X, Y;
         Scanner input =new Scanner(System.in);
         while (((X = input.nextInt()) > 0 )&&((Y = input.nextInt()) > 0 )) {
             int sum = 0;
             if (X > Y) {
-                for (Y = Y; Y <= X; Y++) {
+                for (Y ++; Y <= X; Y++) {
                     sum+=Y;
                     System.out.print(Y+" ");
                 }
                 System.out.print("Sum="+sum+"\n");
             }else {
-                for (X = X; X <= Y; X++) {
+                for (X = i; X <= Y; X++) {
                     sum+=X;
                     System.out.print(X+" ");
                 }
